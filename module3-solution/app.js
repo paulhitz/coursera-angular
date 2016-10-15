a946491ad49e39176ffb088f698ace845783c68a
Paul Hitz <hitzp@dnb.com>
2016-10-15 20:06:47 +0100

    Assignment 3 solution

---
+++ module3-solution/app.js
@@ -0,0 +1,63 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menuSearch = this;
+
+  menuSearch.search = function(searchTerm) {
+    if (searchTerm) {
+      var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+      promise.then(function (response) {
+        menuSearch.found = response;
+      })
+      .catch(function (error) {
+        console.log(error);
+        menuSearch.found = [];
+      });
+    } else {
+      menuSearch.found = [];
+    }
+  };
+
+  menuSearch.removeItem = function(index) {
+    menuSearch.found.splice(index, 1);
+  };
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    });
+
+    return response.then(function (result) {
+        // process result and only keep items that match
+        var foundItems = [];
+        var menuItems = result.data.menu_items;
+        for (var i = 0; i < menuItems.length; i++) {
+          var description = menuItems[i].description.toLowerCase();
+          if (description.indexOf(searchTerm.toLowerCase()) !== -1) {
+            foundItems.push(menuItems[i]);
+          }
+        }
+
+        // return processed items
+        return foundItems;
+    });
+  };
+}
+
+})();
